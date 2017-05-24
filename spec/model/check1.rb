6134e44fdbecb1c40354789c63f0a7ee28bb09c3
Michael Richardson <mcr@sandelman.ca>
2017-05-24 19:24:56 -0400

    validation that the derived-y matches the published y -- requires correct base64 urlsafe decoding

--- spec/model/check1.rb
+++ spec/model/check1.rb
@@ -10,10 +10,14 @@ sig01_key_base64 = {
     d:"V8kgd2ZBRuh2dgyVINBUqpPDr7BOMGcF22CQMIUHtNM"
 }
 
-private_key = ECDSA::Format::IntegerOctetString.decode(Base64.decode64(sig01_key_base64[:d]))
+private_key = ECDSA::Format::IntegerOctetString.decode(Base64.urlsafe_decode64(sig01_key_base64[:d]))
 
-bx=ECDSA::Format::IntegerOctetString.decode(Base64.decode64(sig01_key_base64[:x]))
-by=ECDSA::Format::IntegerOctetString.decode(Base64.decode64(sig01_key_base64[:y]))
+bx=ECDSA::Format::IntegerOctetString.decode(Base64.urlsafe_decode64(sig01_key_base64[:x]))
+basey=Base64.urlsafe_decode64(sig01_key_base64[:y])
+encodey=Base64.encode64(basey)
+
+by=ECDSA::Format::IntegerOctetString.decode(basey)
+
 group        = ECDSA::Group::Nistp256
 sig01_pub_key= group.new_point([bx, by])
 
@@ -21,6 +25,13 @@ public_key  = group.generator.multiply_by_scalar(private_key)
 
 puts "derived x: #{public_key.x} "
 puts "       vs: #{sig01_pub_key.x}"
+
 puts "derived y: #{public_key.y} "
 puts "       vs: #{sig01_pub_key.y}"
+
+puts "y bits: #{public_key.y.to_s(2)}"
+puts "y bits: #{sig01_pub_key.y.to_s(2)}"
+
+puts "basey: #{sig01_key_base64[:y]}"
+puts "encoy: #{encodey}"
 
